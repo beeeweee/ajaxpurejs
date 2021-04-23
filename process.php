352a136fafa57d3b31e768a9786e2d7c33f4ca1a
beeeweee <billdarnalljr@gmail.com>
2021-04-23 11:48:03 -0700

    Post and Fetch Req with AJAX PHP and MySql

--- process.php
+++ process.php
@@ -1,12 +1,25 @@
 <?php
 
+// Connect to a database
+$conn = mysqli_connect('localhost', 'root', 'root', 'ajaxtest', '8889');
+
 echo 'processing....';
 
 
 //Check for get variable
 /// will need security variables for security - must learn
 if(isset($_POST['name'])){
+    //for security
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
     echo 'POST: Your name is '. $_POST['name'];
+
+    $query = "INSERT INTO users(name) VALUES('$name')";
+
+    if(mysqli_query($conn, $query)){
+        echo " User Added...";
+    } else {
+        echo 'Error :'. mysqli_error($conn);
+    }
 
 }
 
