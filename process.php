9db9674dc8a3b0205d2fc7d20417f9524d360f1e
beeeweee <billdarnalljr@gmail.com>
2021-04-23 11:15:25 -0700

    POst req w/ Ajax and PHP

--- process.php
+++ process.php
@@ -2,6 +2,14 @@
 
 echo 'processing....';
 
+
+//Check for get variable
+/// will need security variables for security - must learn
+if(isset($_POST['name'])){
+    echo 'POST: Your name is '. $_POST['name'];
+
+}
+
 //Check for get variable
 /// will need security variables for security - must learn
 if(isset($_GET['name'])){
